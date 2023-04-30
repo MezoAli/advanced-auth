88095489d13c06fe5c8713cbc7d9b5b39827f5ad
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-04-30 13:02:24 +0200

    adding mongodb adapter to store users into database

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
+import clientPromise from "@/lib/mongodb";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+	adapter: MongoDBAdapter(clientPromise),
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID as string,
